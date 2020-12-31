843d3b459fa8698bc7bc75481223e555710f09e3
zyhongcha <zyhongcha@gmail.com>
2020-12-31 03:37:31 +0100

    first commit

---
+++ strapi/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'fa95871dc69569c30b6cb5ee47ae9bb9'),
+    },
+  },
+});
