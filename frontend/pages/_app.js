db42434baad48e69684ee616f7bc1c56ab7a7020
zyhongcha <zyhongcha@gmail.com>
2021-03-27 00:32:01 +0100

    added seo meta tags

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -7,10 +7,15 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
     <Head>
+    <meta charset="UTF-8" />
+    <meta name="description" content="Discover and share your tea articles and stories around the world"></meta>
     <meta
             name="viewport"
             content="width=device-width, initial-scale=1.0, user-scalable=yes"
           />
+
+    <meta name="robots" content="index, follow" />
+    <link rel="canonical" href="https://www.journaloftea.org/" />
     </Head>
     <Layout>
     <Header />
