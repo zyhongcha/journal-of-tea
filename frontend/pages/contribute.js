e2c54f963d32769fcfc1675a38e7bba0f40aea0f
zyhongcha <zyhongcha@gmail.com>
2021-03-28 18:51:57 +0200

    added contribute page

---
+++ frontend/pages/contribute.js
@@ -0,0 +1,13 @@
+
+const Contribute = () => {
+    
+    
+    return (
+        <>
+        <h1>Write and share your tea ideas with other enthusiasts, for free.</h1>
+
+        </>
+        
+    )
+}
+export default Contribute
