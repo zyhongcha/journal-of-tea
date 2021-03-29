d87a6ad27e2ea1af60dfc97d389428c2946a04ca
zyhongcha <zyhongcha@gmail.com>
2021-03-29 23:00:16 +0200

    added wip contribute page

--- frontend/pages/contribute.js
+++ frontend/pages/contribute.js
@@ -4,7 +4,12 @@ const Contribute = () => {
     
     return (
         <>
-        <h1>Write and share your tea ideas with other enthusiasts, for free.</h1>
+        <h1>Coming soon: Write and share your tea ideas for your fellow tea enthusiasts.</h1>
+        <h3>This project journaloftea.org is currently under construction,
+             as we aim to rebuild this website into a public community-driven tea blog publishing platform.
+            If you are interested in our project and would like to contribute, please contact me via <a href="mailto:zyhongcha@gmail.com">zyhongcha@gmail.com</a>.
+            This project is open-source and I welcome any tea enthuasist and developer in supporting us :)  
+        </h3>
 
         </>
         
